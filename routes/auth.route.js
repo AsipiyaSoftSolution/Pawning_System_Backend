d1c6fb1c30102cb1252287c3ffc7c041fa4c0809
Sachinthana Jayathunga <spiyumal48@gmail.com>
2025-08-15 10:39:05 +0530

    implement check auth and make reusable return user fucntion

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { login, logout } from "../controllers/auth.controller.js";
+import { login, logout, checkAuth } from "../controllers/auth.controller.js";
+import { protectedRoute } from "@/middlewares/auth.middleware.js";
 const route = express.Router();
 
 route.post("/login", login);
 route.post("/logout", logout);
+route.get("/check-auth", protectedRoute, checkAuth); // Protected route to check authentication and return user info if local storage is cleared
 export default route;

066dcf65b60bbb2fbe9f6c84bc007b7e314405f6
nethmiTK <nethmitk33@gmail.com>
2025-08-16 21:23:49 +0530

    visibily card setting save

--- routes/ui.setting.route.js
+++ routes/ui.setting.route.js
@@ -9,9 +9,9 @@ import { protectedRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/dashboard-cards/:branchId", protectedRoute, getDashboardCardVisibility);
+router.get("/dashboard-cards/:branchId/:companyId", protectedRoute, getDashboardCardVisibility);
 // Update single dashboard card visibility setting
-router.put("/dashboard-cards/:branchId", protectedRoute, updateDashboardCardVisibility);
+router.put("/dashboard-cards/:branchId/:companyId", protectedRoute, updateDashboardCardVisibility);
 // Reset all dashboard card visibility settings for a user in a branch
 router.delete("/dashboard-cards/:branchId/reset", protectedRoute, resetDashboardCardVisibility);
 
